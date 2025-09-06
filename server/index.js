1c39f18b55ac40730a1081ca00b593204f783a08
Daniel Urbina <edu211004@gmail.com>
2025-09-05 21:11:10 -0600

    get Token Route

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
+import express from 'express'
+import {getToken} from '../src/api/auth.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
 const app = express()
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on http://localhost:${process.env.PORT}`)
+app.get('/api/token', async (req, res) => {
+  const tokenObject = await getToken(process.env.CLIENT_ID, process.env.CLIENT_SECRET)
+  res.json(tokenObject)
+})
+
+app.listen(4001, () => {
+  console.log(`Server running on http://localhost:4001`)
 })
