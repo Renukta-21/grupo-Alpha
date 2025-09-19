ee2396b9916482ed7a6626a4b2bdb8a3f9a3e933
Daniel Urbina <edu211004@gmail.com>
2025-09-19 09:55:21 -0600

    retrieving categories

--- server/index.js
+++ server/index.js
@@ -1,24 +1,18 @@
 const express = require("express")
 const config = require("../server/config")
 const { getCategories, getCategoryItems } = require("./api/syscomAPI")
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.get("", (req,res)=>{
   res.send("Main route")
 })
 
-/* app.get("/api/token", async(req,res)=>{
-  const response = await getToken()
-  if(response.access_token) {
-    token = response.access_token
-    setAPIToken(token)
-  }
-  res.send(response)
-})
- */
 app.get("/categories", async(req,res)=>{
   const response = await getCategories()
+  console.log(response)
   res.send(response)
 })
 app.get("/categories/:id",async(req,res)=>{
